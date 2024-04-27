e5667db6c4c3aec1b55d6464be3119be743aa505
Sean <118757050+Sean-Boyes@users.noreply.github.com>
2024-04-27 14:51:59 -0700

    Create WINpack.bat

---
+++ src/WINpack.bat
@@ -0,0 +1,7 @@
+pyinstaller src\main.py --noconfirm ^
+    --add-data="src\database.db:." ^
+    --add-data="src\config.yaml:." ^
+    --add-data="src\anime-titles.dat:." ^
+    --contents-directory="WIN" ^
+    --name="Epifetch" ^
+    --console
